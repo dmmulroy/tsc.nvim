47b8d54659e9a5eb376c0d10ea5de0779211f4a2
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-05-05 09:43:58 -0400

    fix is_executable logic

--- lua/tsc/init.lua
+++ lua/tsc/init.lua
@@ -91,7 +91,7 @@ M.run = function()
   local notify_called = false
   local spinner_idx = 0
 
-  if utils.is_executable(cmd) then
+  if not utils.is_executable(cmd) then
     vim.notify(
       format_notification_msg(
         "tsc was not available or found in your node_modules or $PATH. Please run install and try again."
