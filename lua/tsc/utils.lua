1d25883d3c5335b0d939a56cd1a394b6d090020c
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-05-06 10:09:06 -0400

    debugging

--- lua/tsc/utils.lua
+++ lua/tsc/utils.lua
@@ -5,9 +5,9 @@ M.is_executable = function(cmd)
 end
 
 M.get_tsc_cmd = function()
-  vim.cmd("silent cd %:h")
+  -- vim.cmd("silent cd %:h")
   local node_modules_tsc_binary = vim.fn.findfile("node_modules/.bin/tsc", ".;")
-  vim.cmd("silent cd-")
+  -- vim.cmd("silent cd-")
   if node_modules_tsc_binary ~= "" then
     return node_modules_tsc_binary
   end
